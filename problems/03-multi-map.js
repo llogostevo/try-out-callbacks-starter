b283ddbabf09d674338b3c99d14d4ff583f1cc3d
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-10-27 10:27:07 +0100

    problem 3 complete

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,11 +21,34 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
-
+let multiMap = function(value, n, callback) {
+  // set the running total to be calculated as the initial value
+  let result = value;
+  
+  // loop through n times, so that the callback can be carried out n times
+  for (let i=0; i<n; i++){
+    // pass in the initial value
+    // then reset the result, passing in the new result value  to the callback so this repeats n times
+    result = (callback(result)); 
+  }
+  // return the result so that it comes back having been passed through the call back n times
+  return result;
 };
 
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
 
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
